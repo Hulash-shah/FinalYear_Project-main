29cfb883d6be067f3dbc99fa096b185f128c80b3
buny10 <shahhulash@gmail.com>
2026-09-06 21:52:09 +0545

    fix: update CORS origin to remove trailing slash

--- Back_End/server.js
+++ Back_End/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "https://bizcore-peach.vercel.app/",
+  origin: "https://bizcore-peach.vercel.app",
   credentials: true
 }));
 
