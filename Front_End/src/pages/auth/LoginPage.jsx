239bf050ecb0bc286ff2ecb7ad2f3860d2edfa0a
buny10 <shahhulash@gmail.com>
2026-09-06 21:32:21 +0545

    update API endpoints in LoginPage and RegisterPage to use production URL

--- Front_End/src/pages/auth/LoginPage.jsx
+++ Front_End/src/pages/auth/LoginPage.jsx
@@ -13,7 +13,7 @@ export default function LoginPage({ onLogin, onNavigate, isDark, toggleTheme }) 
     setError("");
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch("https://finalyear-project-main.onrender.com/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
--- Front_End/src/pages/auth/RegisterPage.jsx
+++ Front_End/src/pages/auth/RegisterPage.jsx
@@ -47,7 +47,7 @@ export default function RegisterPage({  onNavigate }) {
     try {
       setLoading(true);
 
-      const response = await fetch('http://localhost:5000/register', {
+      const response = await fetch('https://finalyear-project-main.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
